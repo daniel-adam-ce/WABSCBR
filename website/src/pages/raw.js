95232d5cb164de620adfa5b131946a87b2abfef2
daniel-adam-ce <danieladamce@gmail.com>
2022-01-27 01:07:20 -0800

    Raw data table

--- website/src/pages/raw.js
+++ website/src/pages/raw.js
@@ -1,11 +1,36 @@
 import React from 'react'
 import {useState} from 'react'
+import Table from 'react-bootstrap/Table'
 
 const RawPage = () => {
+    const [table, setTable] = useState([])
+    const displayTable = () => {
+        return table.map((item, index) => (
+            <tr key={index}>
+                <td>{item.dateReceived}</td>
+                <td>{item.arbId}</td>
+                <td>{item.payload}</td>
+            </tr>
+        ))
+    }
     return (
-        <div>
-            raw
+        <div style={{paddingTop:'1rem'}}>
+            <Table striped bordered hover variant="dark">
+                
+                <thead>
+                    <tr>
+                        <th>Date Received</th>
+                        <th>Arbitration ID</th>
+                        <th>Payload Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {displayTable()}
+                </tbody>
+                
+            </Table>
         </div>
+        
     )
 }
 
