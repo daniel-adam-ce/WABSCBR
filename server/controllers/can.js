381d42487cd4663ae188b9c6d7c08123d7f0826e
daniel-adam-ce <danieladamce@gmail.com>
2022-01-27 17:51:36 -0800

    Placeholder decryption child process
    See comments in added code

--- server/controllers/can.js
+++ server/controllers/can.js
@@ -1,5 +1,5 @@
 import CanData from '../models/can.js'
-import bcrypt from 'bcrypt'
+import {spawn} from 'child_process'
 
 export const getCAN = async (req, res) => {
     try {
@@ -38,6 +38,24 @@ export const deleteCAN = async (req, res) => {
 export const createCAN = async (req, res) => { 
     try {
         const can = req.body
+        
+        // decrypt payload, may not accurately reflect how this will be handled
+        // encryption/decryption is not developed yet
+        // --------------------------------------------------------------
+        // const AEScpp = spawn('../../AEScpp/AEScpp.exe', [can.payload])
+        // let temp_stdout = 0
+        // AEScpp.stdout.on('data', function(data){ 
+        //     temp_stdout = data
+        // })
+        // AEScpp.on('close', (code)=>{
+        //     if(code == 0) {
+        //         can.payload = temp_stdout
+        //     } else {
+        //         throw new Error("Non zero exit from AES decryption")
+        //     }
+        // })
+        // --------------------------------------------------------------
+
         const newCAN = new CanData(can)
         await newCAN.save()
         res.status(200).json(newCAN)
