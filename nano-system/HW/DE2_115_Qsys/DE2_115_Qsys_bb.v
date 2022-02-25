850086a078ec607c1bd4dc6b840990144855048d
MNienaber831 <micaelanienaber@yahoo.com>
2022-02-25 10:46:49 -0800

    attempted to test USB system

--- nano-system/HW/DE2_115_Qsys/DE2_115_Qsys_bb.v
+++ nano-system/HW/DE2_115_Qsys/DE2_115_Qsys_bb.v
@@ -1,7 +1,22 @@
 
 module DE2_115_Qsys (
+	button_pio_external_connection_export,
 	clk_100_clk_in_clk,
 	clk_100_clk_in_reset_reset_n,
+	clk_io_clk_in_clk,
+	cy7c67200_if_0_conduit_end_DATA,
+	cy7c67200_if_0_conduit_end_ADDR,
+	cy7c67200_if_0_conduit_end_RD_N,
+	cy7c67200_if_0_conduit_end_WR_N,
+	cy7c67200_if_0_conduit_end_CS_N,
+	cy7c67200_if_0_conduit_end_RST_N,
+	cy7c67200_if_0_conduit_end_INT,
+	lcd_16207_0_external_RS,
+	lcd_16207_0_external_RW,
+	lcd_16207_0_external_data,
+	lcd_16207_0_external_E,
+	led_green_external_connection_export,
+	led_red_external_connection_export,
 	sdram_0_wire_addr,
 	sdram_0_wire_ba,
 	sdram_0_wire_cas_n,
@@ -11,14 +26,6 @@ module DE2_115_Qsys (
 	sdram_0_wire_dqm,
 	sdram_0_wire_ras_n,
 	sdram_0_wire_we_n,
-	lcd_16207_0_external_RS,
-	lcd_16207_0_external_RW,
-	lcd_16207_0_external_data,
-	lcd_16207_0_external_E,
-	led_red_external_connection_export,
-	led_green_external_connection_export,
-	button_pio_external_connection_export,
-	switch_pio_external_connection_export,
 	seg7_display_conduit_end_oSEG0,
 	seg7_display_conduit_end_oSEG1,
 	seg7_display_conduit_end_oSEG2,
@@ -27,17 +34,25 @@ module DE2_115_Qsys (
 	seg7_display_conduit_end_oSEG5,
 	seg7_display_conduit_end_oSEG6,
 	seg7_display_conduit_end_oSEG7,
-	cy7c67200_if_0_conduit_end_DATA,
-	cy7c67200_if_0_conduit_end_ADDR,
-	cy7c67200_if_0_conduit_end_RD_N,
-	cy7c67200_if_0_conduit_end_WR_N,
-	cy7c67200_if_0_conduit_end_CS_N,
-	cy7c67200_if_0_conduit_end_RST_N,
-	cy7c67200_if_0_conduit_end_INT,
-	clk_io_clk_in_clk);	
+	switch_pio_external_connection_export);	
 
+	input	[3:0]	button_pio_external_connection_export;
 	input		clk_100_clk_in_clk;
 	input		clk_100_clk_in_reset_reset_n;
+	input		clk_io_clk_in_clk;
+	inout	[15:0]	cy7c67200_if_0_conduit_end_DATA;
+	output	[1:0]	cy7c67200_if_0_conduit_end_ADDR;
+	output		cy7c67200_if_0_conduit_end_RD_N;
+	output		cy7c67200_if_0_conduit_end_WR_N;
+	output		cy7c67200_if_0_conduit_end_CS_N;
+	output		cy7c67200_if_0_conduit_end_RST_N;
+	input		cy7c67200_if_0_conduit_end_INT;
+	output		lcd_16207_0_external_RS;
+	output		lcd_16207_0_external_RW;
+	inout	[7:0]	lcd_16207_0_external_data;
+	output		lcd_16207_0_external_E;
+	output	[8:0]	led_green_external_connection_export;
+	output	[17:0]	led_red_external_connection_export;
 	output	[12:0]	sdram_0_wire_addr;
 	output	[1:0]	sdram_0_wire_ba;
 	output		sdram_0_wire_cas_n;
@@ -47,14 +62,6 @@ module DE2_115_Qsys (
 	output	[3:0]	sdram_0_wire_dqm;
 	output		sdram_0_wire_ras_n;
 	output		sdram_0_wire_we_n;
-	output		lcd_16207_0_external_RS;
-	output		lcd_16207_0_external_RW;
-	inout	[7:0]	lcd_16207_0_external_data;
-	output		lcd_16207_0_external_E;
-	output	[17:0]	led_red_external_connection_export;
-	output	[8:0]	led_green_external_connection_export;
-	input	[3:0]	button_pio_external_connection_export;
-	input	[17:0]	switch_pio_external_connection_export;
 	output	[6:0]	seg7_display_conduit_end_oSEG0;
 	output	[6:0]	seg7_display_conduit_end_oSEG1;
 	output	[6:0]	seg7_display_conduit_end_oSEG2;
@@ -63,12 +70,5 @@ module DE2_115_Qsys (
 	output	[6:0]	seg7_display_conduit_end_oSEG5;
 	output	[6:0]	seg7_display_conduit_end_oSEG6;
 	output	[6:0]	seg7_display_conduit_end_oSEG7;
-	inout	[15:0]	cy7c67200_if_0_conduit_end_DATA;
-	output	[1:0]	cy7c67200_if_0_conduit_end_ADDR;
-	output		cy7c67200_if_0_conduit_end_RD_N;
-	output		cy7c67200_if_0_conduit_end_WR_N;
-	output		cy7c67200_if_0_conduit_end_CS_N;
-	output		cy7c67200_if_0_conduit_end_RST_N;
-	input		cy7c67200_if_0_conduit_end_INT;
-	input		clk_io_clk_in_clk;
+	input	[17:0]	switch_pio_external_connection_export;
 endmodule
--- nano-system/HW/DE2_115_Qsys/DE2_115_Qsys_inst.vhd
+++ nano-system/HW/DE2_115_Qsys/DE2_115_Qsys_inst.vhd
@@ -1,7 +1,22 @@
 	component DE2_115_Qsys is
 		port (
+			button_pio_external_connection_export : in    std_logic_vector(3 downto 0)  := (others => 'X'); -- export
 			clk_100_clk_in_clk                    : in    std_logic                     := 'X';             -- clk
 			clk_100_clk_in_reset_reset_n          : in    std_logic                     := 'X';             -- reset_n
+			clk_io_clk_in_clk                     : in    std_logic                     := 'X';             -- clk
+			cy7c67200_if_0_conduit_end_DATA       : inout std_logic_vector(15 downto 0) := (others => 'X'); -- DATA
+			cy7c67200_if_0_conduit_end_ADDR       : out   std_logic_vector(1 downto 0);                     -- ADDR
+			cy7c67200_if_0_conduit_end_RD_N       : out   std_logic;                                        -- RD_N
+			cy7c67200_if_0_conduit_end_WR_N       : out   std_logic;                                        -- WR_N
+			cy7c67200_if_0_conduit_end_CS_N       : out   std_logic;                                        -- CS_N
+			cy7c67200_if_0_conduit_end_RST_N      : out   std_logic;                                        -- RST_N
+			cy7c67200_if_0_conduit_end_INT        : in    std_logic                     := 'X';             -- INT
+			lcd_16207_0_external_RS               : out   std_logic;                                        -- RS
+			lcd_16207_0_external_RW               : out   std_logic;                                        -- RW
+			lcd_16207_0_external_data             : inout std_logic_vector(7 downto 0)  := (others => 'X'); -- data
+			lcd_16207_0_external_E                : out   std_logic;                                        -- E
+			led_green_external_connection_export  : out   std_logic_vector(8 downto 0);                     -- export
+			led_red_external_connection_export    : out   std_logic_vector(17 downto 0);                    -- export
 			sdram_0_wire_addr                     : out   std_logic_vector(12 downto 0);                    -- addr
 			sdram_0_wire_ba                       : out   std_logic_vector(1 downto 0);                     -- ba
 			sdram_0_wire_cas_n                    : out   std_logic;                                        -- cas_n
@@ -11,14 +26,6 @@
 			sdram_0_wire_dqm                      : out   std_logic_vector(3 downto 0);                     -- dqm
 			sdram_0_wire_ras_n                    : out   std_logic;                                        -- ras_n
 			sdram_0_wire_we_n                     : out   std_logic;                                        -- we_n
-			lcd_16207_0_external_RS               : out   std_logic;                                        -- RS
-			lcd_16207_0_external_RW               : out   std_logic;                                        -- RW
-			lcd_16207_0_external_data             : inout std_logic_vector(7 downto 0)  := (others => 'X'); -- data
-			lcd_16207_0_external_E                : out   std_logic;                                        -- E
-			led_red_external_connection_export    : out   std_logic_vector(17 downto 0);                    -- export
-			led_green_external_connection_export  : out   std_logic_vector(8 downto 0);                     -- export
-			button_pio_external_connection_export : in    std_logic_vector(3 downto 0)  := (others => 'X'); -- export
-			switch_pio_external_connection_export : in    std_logic_vector(17 downto 0) := (others => 'X'); -- export
 			seg7_display_conduit_end_oSEG0        : out   std_logic_vector(6 downto 0);                     -- oSEG0
 			seg7_display_conduit_end_oSEG1        : out   std_logic_vector(6 downto 0);                     -- oSEG1
 			seg7_display_conduit_end_oSEG2        : out   std_logic_vector(6 downto 0);                     -- oSEG2
@@ -27,21 +34,29 @@
 			seg7_display_conduit_end_oSEG5        : out   std_logic_vector(6 downto 0);                     -- oSEG5
 			seg7_display_conduit_end_oSEG6        : out   std_logic_vector(6 downto 0);                     -- oSEG6
 			seg7_display_conduit_end_oSEG7        : out   std_logic_vector(6 downto 0);                     -- oSEG7
-			cy7c67200_if_0_conduit_end_DATA       : inout std_logic_vector(15 downto 0) := (others => 'X'); -- DATA
-			cy7c67200_if_0_conduit_end_ADDR       : out   std_logic_vector(1 downto 0);                     -- ADDR
-			cy7c67200_if_0_conduit_end_RD_N       : out   std_logic;                                        -- RD_N
-			cy7c67200_if_0_conduit_end_WR_N       : out   std_logic;                                        -- WR_N
-			cy7c67200_if_0_conduit_end_CS_N       : out   std_logic;                                        -- CS_N
-			cy7c67200_if_0_conduit_end_RST_N      : out   std_logic;                                        -- RST_N
-			cy7c67200_if_0_conduit_end_INT        : in    std_logic                     := 'X';             -- INT
-			clk_io_clk_in_clk                     : in    std_logic                     := 'X'              -- clk
+			switch_pio_external_connection_export : in    std_logic_vector(17 downto 0) := (others => 'X')  -- export
 		);
 	end component DE2_115_Qsys;
 
 	u0 : component DE2_115_Qsys
 		port map (
+			button_pio_external_connection_export => CONNECTED_TO_button_pio_external_connection_export, -- button_pio_external_connection.export
 			clk_100_clk_in_clk                    => CONNECTED_TO_clk_100_clk_in_clk,                    --                 clk_100_clk_in.clk
 			clk_100_clk_in_reset_reset_n          => CONNECTED_TO_clk_100_clk_in_reset_reset_n,          --           clk_100_clk_in_reset.reset_n
+			clk_io_clk_in_clk                     => CONNECTED_TO_clk_io_clk_in_clk,                     --                  clk_io_clk_in.clk
+			cy7c67200_if_0_conduit_end_DATA       => CONNECTED_TO_cy7c67200_if_0_conduit_end_DATA,       --     cy7c67200_if_0_conduit_end.DATA
+			cy7c67200_if_0_conduit_end_ADDR       => CONNECTED_TO_cy7c67200_if_0_conduit_end_ADDR,       --                               .ADDR
+			cy7c67200_if_0_conduit_end_RD_N       => CONNECTED_TO_cy7c67200_if_0_conduit_end_RD_N,       --                               .RD_N
+			cy7c67200_if_0_conduit_end_WR_N       => CONNECTED_TO_cy7c67200_if_0_conduit_end_WR_N,       --                               .WR_N
+			cy7c67200_if_0_conduit_end_CS_N       => CONNECTED_TO_cy7c67200_if_0_conduit_end_CS_N,       --                               .CS_N
+			cy7c67200_if_0_conduit_end_RST_N      => CONNECTED_TO_cy7c67200_if_0_conduit_end_RST_N,      --                               .RST_N
+			cy7c67200_if_0_conduit_end_INT        => CONNECTED_TO_cy7c67200_if_0_conduit_end_INT,        --                               .INT
+			lcd_16207_0_external_RS               => CONNECTED_TO_lcd_16207_0_external_RS,               --           lcd_16207_0_external.RS
+			lcd_16207_0_external_RW               => CONNECTED_TO_lcd_16207_0_external_RW,               --                               .RW
+			lcd_16207_0_external_data             => CONNECTED_TO_lcd_16207_0_external_data,             --                               .data
+			lcd_16207_0_external_E                => CONNECTED_TO_lcd_16207_0_external_E,                --                               .E
+			led_green_external_connection_export  => CONNECTED_TO_led_green_external_connection_export,  --  led_green_external_connection.export
+			led_red_external_connection_export    => CONNECTED_TO_led_red_external_connection_export,    --    led_red_external_connection.export
 			sdram_0_wire_addr                     => CONNECTED_TO_sdram_0_wire_addr,                     --                   sdram_0_wire.addr
 			sdram_0_wire_ba                       => CONNECTED_TO_sdram_0_wire_ba,                       --                               .ba
 			sdram_0_wire_cas_n                    => CONNECTED_TO_sdram_0_wire_cas_n,                    --                               .cas_n
@@ -51,14 +66,6 @@
 			sdram_0_wire_dqm                      => CONNECTED_TO_sdram_0_wire_dqm,                      --                               .dqm
 			sdram_0_wire_ras_n                    => CONNECTED_TO_sdram_0_wire_ras_n,                    --                               .ras_n
 			sdram_0_wire_we_n                     => CONNECTED_TO_sdram_0_wire_we_n,                     --                               .we_n
-			lcd_16207_0_external_RS               => CONNECTED_TO_lcd_16207_0_external_RS,               --           lcd_16207_0_external.RS
-			lcd_16207_0_external_RW               => CONNECTED_TO_lcd_16207_0_external_RW,               --                               .RW
-			lcd_16207_0_external_data             => CONNECTED_TO_lcd_16207_0_external_data,             --                               .data
-			lcd_16207_0_external_E                => CONNECTED_TO_lcd_16207_0_external_E,                --                               .E
-			led_red_external_connection_export    => CONNECTED_TO_led_red_external_connection_export,    --    led_red_external_connection.export
-			led_green_external_connection_export  => CONNECTED_TO_led_green_external_connection_export,  --  led_green_external_connection.export
-			button_pio_external_connection_export => CONNECTED_TO_button_pio_external_connection_export, -- button_pio_external_connection.export
-			switch_pio_external_connection_export => CONNECTED_TO_switch_pio_external_connection_export, -- switch_pio_external_connection.export
 			seg7_display_conduit_end_oSEG0        => CONNECTED_TO_seg7_display_conduit_end_oSEG0,        --       seg7_display_conduit_end.oSEG0
 			seg7_display_conduit_end_oSEG1        => CONNECTED_TO_seg7_display_conduit_end_oSEG1,        --                               .oSEG1
 			seg7_display_conduit_end_oSEG2        => CONNECTED_TO_seg7_display_conduit_end_oSEG2,        --                               .oSEG2
@@ -67,13 +74,6 @@
 			seg7_display_conduit_end_oSEG5        => CONNECTED_TO_seg7_display_conduit_end_oSEG5,        --                               .oSEG5
 			seg7_display_conduit_end_oSEG6        => CONNECTED_TO_seg7_display_conduit_end_oSEG6,        --                               .oSEG6
 			seg7_display_conduit_end_oSEG7        => CONNECTED_TO_seg7_display_conduit_end_oSEG7,        --                               .oSEG7
-			cy7c67200_if_0_conduit_end_DATA       => CONNECTED_TO_cy7c67200_if_0_conduit_end_DATA,       --     cy7c67200_if_0_conduit_end.DATA
-			cy7c67200_if_0_conduit_end_ADDR       => CONNECTED_TO_cy7c67200_if_0_conduit_end_ADDR,       --                               .ADDR
-			cy7c67200_if_0_conduit_end_RD_N       => CONNECTED_TO_cy7c67200_if_0_conduit_end_RD_N,       --                               .RD_N
-			cy7c67200_if_0_conduit_end_WR_N       => CONNECTED_TO_cy7c67200_if_0_conduit_end_WR_N,       --                               .WR_N
-			cy7c67200_if_0_conduit_end_CS_N       => CONNECTED_TO_cy7c67200_if_0_conduit_end_CS_N,       --                               .CS_N
-			cy7c67200_if_0_conduit_end_RST_N      => CONNECTED_TO_cy7c67200_if_0_conduit_end_RST_N,      --                               .RST_N
-			cy7c67200_if_0_conduit_end_INT        => CONNECTED_TO_cy7c67200_if_0_conduit_end_INT,        --                               .INT
-			clk_io_clk_in_clk                     => CONNECTED_TO_clk_io_clk_in_clk                      --                  clk_io_clk_in.clk
+			switch_pio_external_connection_export => CONNECTED_TO_switch_pio_external_connection_export  -- switch_pio_external_connection.export
 		);
 
