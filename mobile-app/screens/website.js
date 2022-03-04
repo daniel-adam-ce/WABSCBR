2ed4e61c3f3a88d56d5702b7d15ba31a58eaca16
daniel-adam-ce <danieladamce@gmail.com>
2022-03-03 21:09:47 -0800

    Website embed
    WebView takes in token from login screen. Login screen needs to be properly styled still.

---
+++ mobile-app/screens/website.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { useState, useEffect } from 'react';
+import { WebView } from 'react-native-webview';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+const WebsiteScreen = ({navigation}) => {
+    const [loadState, setLoadState] = useState(true)
+    const [token, setToken] = useState(null)
+    const loadToken = async () => {
+      try {
+        const user = await AsyncStorage.getItem('user')
+        setToken(JSON.parse(user).token)
+        console.log('storage: ', token)
+        setLoadState(false)
+      } catch (error) {
+        console.log(error) 
+      }
+    }
+    useEffect(()=> {
+      
+      loadToken()
+    }, [])
+    return (
+      <>
+      {!loadState && 
+      <WebView
+        source={{
+            uri: `http://10.0.0.214:5001/auth?token=${token}`
+        }}
+      />}
+      </>
+    )
+}
+
+
+export default WebsiteScreen
