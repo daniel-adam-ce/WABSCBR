676bc12576aea08fc0b98695b9c029c038284b4a
daniel-adam-ce <danieladamce@gmail.com>
2022-03-06 23:06:05 -0800

    Moved login screen

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,129 +1,18 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-  Button,
-  ImageBackground
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-google-signin/google-signin';
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import HomeScreen from './screens/home.js'
 import BluetoothScreen from './screens/bluetooth.js';
 import WebsiteScreen from './screens/website.js';
+import LoginScreen from './screens/auth.js';
 
-import AsyncStorage from '@react-native-async-storage/async-storage'
-
-GoogleSignin.configure({
-  webClientId: '469403570539-fnk4vhg7v5eb9ta1no0lr5fc24gco4b8.apps.googleusercontent.com', 
-  offlineAccess: true
-})
 const Stack = createNativeStackNavigator()
 
-const signIn = async (navigation) => {
-  try {
-    await GoogleSignin.hasPlayServices();
-    const userInfo = await GoogleSignin.signIn();
-    console.log(userInfo)
-    const user = {
-      token: userInfo.idToken,
-      isGoogle: true,
-      profileObj: userInfo.user
-    }
-    await AsyncStorage.setItem('user', JSON.stringify(user))
-    navigation.navigate('Home')
-    // this.setState({ userInfo });
-  } catch (error) {
-    if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-      // user cancelled the login flow
-    } else if (error.code === statusCodes.IN_PROGRESS) {
-      // operation (e.g. sign in) is in progress already
-    } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-      // play services not available or outdated
-    } else {
-      // some other error happened
-    }
-    console.log(error)
-  }
-};
-
-const Section = ({children, title})=> {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-
-const LoginScreen = ({navigation}) => {
-  return (
-    <View>
-      <Text>login</Text>
-      <GoogleSigninButton
-      style={{width:192, height: 48}}
-      onPress={()=>{
-        signIn(navigation)
-      }}
-      />
-      <Button 
-      title="go to home"
-      onPress={()=>{
-        navigation.navigate('Home')
-      }}
-      />
-    </View>
-  )
-  
-}
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
 
   return (
     <NavigationContainer>
@@ -150,23 +39,5 @@ const App = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
