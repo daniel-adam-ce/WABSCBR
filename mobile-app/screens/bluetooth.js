027ed199a9682e4d302a28a8a54b2e00db939bcd
daniel-adam-ce <danieladamce@gmail.com>
2022-03-06 23:08:12 -0800

    New screen for Bluetooth classic

--- mobile-app/screens/bluetooth.js
+++ mobile-app/screens/bluetooth.js
@@ -10,253 +10,96 @@
     useEffect,
   } from 'react';
   import {
-    SafeAreaView,
     StyleSheet,
-    ScrollView,
+    PermissionsAndroid,
+    Toast,
+    ToastAndroid,
     View,
     Text,
-    StatusBar,
-    NativeModules,
-    NativeEventEmitter,
-    Button,
-    Platform,
-    PermissionsAndroid,
-    FlatList,
-    TouchableHighlight,
+    Pressable,
+    ScrollView
   } from 'react-native';
   
   import {
     Colors,
   } from 'react-native/Libraries/NewAppScreen';
   
-  import BleManager from 'react-native-ble-manager';
-  const BleManagerModule = NativeModules.BleManager;
-  const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
+  import RNBluetoothClassic, {
+    BluetoothDevice
+  } from 'react-native-bluetooth-classic';
   
+  
+  
+
   const BluetoothScreen = ({navigation}) => {
-    const [isScanning, setIsScanning] = useState(false);
-    const peripherals = new Map();
-    const [list, setList] = useState([]);
-  
-  
-    const startScan = () => {
-      if (!isScanning) {
-        BleManager.scan([], 10, false).then((results) => {
-          console.log('Scanning...');
-          setIsScanning(true);
-        }).catch(err => {
-          console.error(err);
-        });
-      }    
-    }
-  
-    const handleStopScan = () => {
-      console.log('Scan is stopped');
-      setIsScanning(false);
-    }
-  
-    const handleDisconnectedPeripheral = (data) => {
-      let peripheral = peripherals.get(data.peripheral);
-      if (peripheral) {
-        peripheral.connected = false;
-        peripherals.set(peripheral.id, peripheral);
-        setList(Array.from(peripherals.values()));
-      }
-      console.log('Disconnected from ' + data.peripheral);
-    }
-  
-    const handleUpdateValueForCharacteristic = (data) => {
-      console.log('Received data from ' + data.peripheral + ' characteristic ' + data.characteristic, data.value);
-    }
-  
-    const retrieveConnected = () => {
-      BleManager.getConnectedPeripherals([]).then((results) => {
-        if (results.length == 0) {
-          console.log('No connected peripherals')
+
+    const [bluetoothState, setBluetoothState] = useState({})
+    
+    const requestAccessFineLocationPermission = async () => {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Access fine location required for discovery',
+          message:
+            'In order to perform discovery, you must enable/allow ' +
+            'fine location access.',
+          buttonNeutral: 'Ask Me Later"',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'OK'
         }
-        console.log(results);
-        for (var i = 0; i < results.length; i++) {
-          var peripheral = results[i];
-          peripheral.connected = true;
-          peripherals.set(peripheral.id, peripheral);
-          setList(Array.from(peripherals.values()));
-        }
-      });
-    }
-  
-    const handleDiscoverPeripheral = (peripheral) => {
-      console.log('Got ble peripheral', peripheral);
-      if (!peripheral.name) {
-        peripheral.name = 'NO NAME';
-      }
-      peripherals.set(peripheral.id, peripheral);
-      setList(Array.from(peripherals.values()));
-    }
-  
-    const testPeripheral = (peripheral) => {
-      if (peripheral){
-        if (peripheral.connected){
-          BleManager.disconnect(peripheral.id);
-        }else{
-          BleManager.connect(peripheral.id).then(() => {
-            let p = peripherals.get(peripheral.id);
-            if (p) {
-              p.connected = true;
-              peripherals.set(peripheral.id, p);
-              setList(Array.from(peripherals.values()));
-            }
-            console.log('Connected to ' + peripheral.id);
-  
-  
-            setTimeout(() => {
-  
-              /* Test read current RSSI value */
-              BleManager.retrieveServices(peripheral.id).then((peripheralData) => {
-                console.log('Retrieved peripheral services', peripheralData);
-  
-                BleManager.readRSSI(peripheral.id).then((rssi) => {
-                  console.log('Retrieved actual RSSI value', rssi);
-                  let p = peripherals.get(peripheral.id);
-                  if (p) {
-                    p.rssi = rssi;
-                    peripherals.set(peripheral.id, p);
-                    setList(Array.from(peripherals.values()));
-                  }                
-                });                                          
-              });
-  
-              // Test using bleno's pizza example
-              // https://github.com/sandeepmistry/bleno/tree/master/examples/pizza
-              /*
-              BleManager.retrieveServices(peripheral.id).then((peripheralInfo) => {
-                console.log(peripheralInfo);
-                var service = '13333333-3333-3333-3333-333333333337';
-                var bakeCharacteristic = '13333333-3333-3333-3333-333333330003';
-                var crustCharacteristic = '13333333-3333-3333-3333-333333330001';
-  
-                setTimeout(() => {
-                  BleManager.startNotification(peripheral.id, service, bakeCharacteristic).then(() => {
-                    console.log('Started notification on ' + peripheral.id);
-                    setTimeout(() => {
-                      BleManager.write(peripheral.id, service, crustCharacteristic, [0]).then(() => {
-                        console.log('Writed NORMAL crust');
-                        BleManager.write(peripheral.id, service, bakeCharacteristic, [1,95]).then(() => {
-                          console.log('Writed 351 temperature, the pizza should be BAKED');
-                          
-                          //var PizzaBakeResult = {
-                          //  HALF_BAKED: 0,
-                          //  BAKED:      1,
-                          //  CRISPY:     2,
-                          //  BURNT:      3,
-                          //  ON_FIRE:    4
-                          //};
-                        });
-                      });
-  
-                    }, 500);
-                  }).catch((error) => {
-                    console.log('Notification error', error);
-                  });
-                }, 200);
-              });*/
-  
-              
-  
-            }, 900);
-          }).catch((error) => {
-            console.log('Connection error', error);
-          });
-        }
-      }
-  
-    }
-  
-    useEffect(() => {
-      BleManager.start({showAlert: false});
-  
-      bleManagerEmitter.addListener('BleManagerDiscoverPeripheral', handleDiscoverPeripheral);
-      bleManagerEmitter.addListener('BleManagerStopScan', handleStopScan );
-      bleManagerEmitter.addListener('BleManagerDisconnectPeripheral', handleDisconnectedPeripheral );
-      bleManagerEmitter.addListener('BleManagerDidUpdateValueForCharacteristic', handleUpdateValueForCharacteristic );
-  
-      if (Platform.OS === 'android' && Platform.Version >= 23) {
-        PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION).then((result) => {
-            if (result) {
-              console.log("Permission is OK");
-            } else {
-              PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION).then((result) => {
-                if (result) {
-                  console.log("User accept");
-                } else {
-                  console.log("User refuse");
-                }
-              });
-            }
-        });
-      }  
-      
-      return (() => {
-        console.log('unmount');
-        bleManagerEmitter.removeListener('BleManagerDiscoverPeripheral', handleDiscoverPeripheral);
-        bleManagerEmitter.removeListener('BleManagerStopScan', handleStopScan );
-        bleManagerEmitter.removeListener('BleManagerDisconnectPeripheral', handleDisconnectedPeripheral );
-        bleManagerEmitter.removeListener('BleManagerDidUpdateValueForCharacteristic', handleUpdateValueForCharacteristic );
-      })
-    }, []);
-  
-    const renderItem = (item) => {
-      const color = item.connected ? 'green' : '#fff';
-      return (
-        <TouchableHighlight onPress={() => testPeripheral(item) }>
-          <View style={[styles.row, {backgroundColor: color}]}>
-            <Text style={{fontSize: 12, textAlign: 'center', color: '#333333', padding: 10}}>{item.name}</Text>
-            <Text style={{fontSize: 10, textAlign: 'center', color: '#333333', padding: 2}}>RSSI: {item.rssi}</Text>
-            <Text style={{fontSize: 8, textAlign: 'center', color: '#333333', padding: 2, paddingBottom: 20}}>{item.id}</Text>
-          </View>
-        </TouchableHighlight>
       );
+      return granted === PermissionsAndroid.RESULTS.GRANTED;
+    };
+    
+    startDiscovery = async () => {
+      try {
+        console.log('starting...')
+        let granted = await requestAccessFineLocationPermission();
+    
+        if (!granted) {
+        throw new Error(`Access fine location was not granted`);
+        }
+    
+        setBluetoothState({ discovering: true });
+    
+        let unpaired = await RNBluetoothClassic.startDiscovery();
+        console.log(`Found ${unpaired.length} unpaired devices.`)
+        ToastAndroid.show(`Found ${unpaired.length} unpaired devices.`, 2000);
+        setBluetoothState({ unpaired, discovering: false });
+        
+
+      } catch (err) {
+        ToastAndroid.show(err.message, 2000);
+      }
     }
+
+    useEffect(()=>{
+      startDiscovery()
+    }, [])
   
     return (
-      <>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView>
-          <ScrollView
-            contentInsetAdjustmentBehavior="automatic"
-            style={styles.scrollView}>
-            {global.HermesInternal == null ? null : (
-              <View style={styles.engine}>
-                <Text style={styles.footer}>Engine: Hermes</Text>
-              </View>
-            )}
-            <View style={styles.body}>
-              
-              <View style={{margin: 10}}>
-                <Button 
-                  title={'Scan Bluetooth (' + (isScanning ? 'on' : 'off') + ')'}
-                  onPress={() => startScan() } 
-                />            
-              </View>
-  
-              <View style={{margin: 10}}>
-                <Button title="Retrieve connected peripherals" onPress={() => retrieveConnected() } />
-              </View>
-  
-              {(list.length == 0) &&
-                <View style={{flex:1, margin: 20}}>
-                  <Text style={{textAlign: 'center'}}>No peripherals</Text>
-                </View>
-              }
-            
-            </View>              
-          </ScrollView>
-          <FlatList
-              data={list}
-              renderItem={({ item }) => renderItem(item) }
-              keyExtractor={item => item.id}
-            />              
-        </SafeAreaView>
-      </>
+        <ScrollView contentContainerStyle={{ flexGrow: 1, alignItems: 'center' }}>
+          {bluetoothState.discovering ? 
+          <Text style={{marginTop: 10, color: 'black'}}>Searching...</Text> :<Text style={{marginTop: 10, color: 'black'}}>Select a device to pair:</Text> }
+          {bluetoothState.unpaired !== undefined && (bluetoothState.unpaired.map((device)=>{
+            return (
+                <Pressable 
+                key={device._nativeDevice.address}
+                style={styles.btn}
+                onPress={async ()=>{
+                  try {
+                    await RNBluetoothClassic.pairDevice(device._nativeDevice.address)
+                  } catch (error) {
+                    console.error(error)
+                  }
+                }}
+                >
+                <Text style={{color: 'white'}}>Address: {device._nativeDevice.address}</Text>
+                <Text style={{color: 'white'}}>Device Name: {device._nativeDevice.name}</Text>
+              </Pressable> 
+            )
+          }))}
+        </ScrollView>
     );
   };
   
@@ -297,6 +140,20 @@
       paddingRight: 12,
       textAlign: 'right',
     },
+    btn: {
+      alignItems: 'center',
+      paddingVertical: 12,
+      paddingHorizontal: 32,
+      borderRadius: 10,
+      // elevation: 10,
+      backgroundColor: '#38a1f1',
+      width: '50%',
+      marginTop: 10,
+      marginBottom: 10,
+      borderStyle: 'solid',
+      borderColor: 'red'
+      // marginLeft: '25%'
+  }
   });
   
   export default BluetoothScreen;
