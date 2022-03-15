7470245c60d4cd204bf1f93f52673b3f1f74fc66
NicholasSKumar <nskumar@mail.fresnostate.edu>
2022-03-14 18:51:39 -0700

    Exe for Child Process

--- aes/decryption/dataOrganize.cpp
+++ aes/decryption/dataOrganize.cpp
@@ -5,34 +5,6 @@
 #include <C:\Users\nicej\Documents\School\Y4S2\186B\WABSCBR\aes\decryption\invSbox.h>
 using namespace std;
 
-/*
-int main(int argc, char** argv){
-    
-    int ciphertxt[16];
-    
-    for (int i = 0; i < 16; i++) {
-        ciphertxt[i] = 0;
-    }
-    std::cout << argv[1] << std::endl;
-    char tempCString[3] = {0};
-    int size = 0;
-    for (int i = 0, j = 0; i < 16; (i=i+2), j++){
-        tempCString[0] = argv[1][i];
-        tempCString[1] = argv[1][i+1];
-        std::cout << tempCString[0] << " " << tempCString[1] << std::endl;
-        ciphertxt[j] = std::strtoul(tempCString, nullptr, 16);
-        size = j;
-    }
-
-    for (int x = 0; x<= size; x++){
-        std::cout << ciphertxt[x] << endl;
-    }
-    
-
-    return 0;
-}
-*/
-
 void printArray(int array[16]){{
     for(int i = 0; i<16; i++){
         cout << std::hex << array[i] << "\t";
@@ -43,37 +15,33 @@ void printArray(int array[16]){{
     return;
 }
 
-int main(){
+
+int main(int argc, char** argv){
+    
     int ciphertxt[16];
-    int words[44][4];
-    ciphertxt[0] = 0xc7 ;
-    ciphertxt[1] = 0xd1 ;
-    ciphertxt[2] = 0x24 ;
-    ciphertxt[3] = 0x19 ;
-    ciphertxt[4] = 0x48 ;
-    ciphertxt[5] = 0x9e ;
-    ciphertxt[6] = 0x3b ;
-    ciphertxt[7] = 0x62 ;
-    ciphertxt[8] = 0x33 ;
-    ciphertxt[9] = 0xa2 ;
-    ciphertxt[10] = 0xc5 ;
-    ciphertxt[11] = 0xa7 ;
-    ciphertxt[12] = 0xf4 ;
-    ciphertxt[13] = 0x56 ;
-    ciphertxt[14] = 0x31 ;
-    ciphertxt[15] = 0x72 ;
-
     int key[16];
-
-    for(int i=0; i<16;i++){
-        key[i] = 0x0;
-        //ciphertxt[i] = 0x0;
+    for (int i = 0; i < 16; i++) {
+        ciphertxt[i] = 0;
+    }
+    std::cout << argv[1] << std::endl;
+    char tempCString[3] = {0};
+    char tempCString2[3] = {0};
+    int size = 0;
+    for (int i = 0, j = 0; i < 16; (i=i+2), j++){
+        tempCString[0] = argv[1][i];
+        tempCString[1] = argv[1][i+1];
+        tempCString2[0] = argv[2][i];
+        tempCString2[1] = argv[2][i+1];
+        std::cout << tempCString[0] << " " << tempCString[1] << std::endl;
+        ciphertxt[j] = std::strtoul(tempCString, nullptr, 16);
+        key[j] = std::strtoul(tempCString2, nullptr, 16);
+        size = j;
     }
 
+    int words[44][4];
+
     keyExpand(words,key);
-
     int exkeys[11][16];
-
     exkeys[0][0] = words[0][0];
     exkeys[0][1] = words[0][1];
     exkeys[0][2] = words[0][2];
@@ -113,43 +81,15 @@ int main(){
         exkeys[i][15] = words[((i*4)+3)][3];
     }
         addRndKey(ciphertxt, exkeys[10]);
-       
-
-
     for(int i=9; i>0; i--){
-        //cout << "Rowhsift Output:" ;
         rowShift(ciphertxt);
-        //printArray(ciphertxt);
-
-        //cout << "SBox Output: " ;
         invSBox(ciphertxt);
-        //printArray(ciphertxt);
-
-        //cout << "AddRoundKey Output:" ;
-        addRndKey(ciphertxt, exkeys[i]);
-        //printArray(ciphertxt);
-
-        //cout << "ColumnMix Output:" ;
+        addRndKey(ciphertxt, exkeys[i]);;
         ColumnMix(ciphertxt);
-        //printArray(ciphertxt);
-
-        //cout << endl;
     }
-
-        //cout << "Rowhsift Output:" ;
         rowShift(ciphertxt);
-        //printArray(ciphertxt);
-
-        //cout << "SBox Output: " ;
         invSBox(ciphertxt);
-        //printArray(ciphertxt);
-
-        //cout << "AddRoundKey Output:" ;
         addRndKey(ciphertxt, exkeys[0]);
-        //printArray(ciphertxt);
-    
-    printArray(ciphertxt);
-
     return 0;
 
 }
