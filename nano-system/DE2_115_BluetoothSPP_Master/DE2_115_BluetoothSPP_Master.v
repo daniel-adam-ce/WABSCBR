34f7f900bc2126e64c0f5740e48dab38259fda27
NicholasSKumar <nskumar@mail.fresnostate.edu>
2022-04-18 18:20:42 -0700

    DE2-115 Master

--- nano-system/DE2_115_BluetoothSPP_Master/DE2_115_BluetoothSPP_Master.v
+++ nano-system/DE2_115_BluetoothSPP_Master/DE2_115_BluetoothSPP_Master.v
@@ -10,140 +10,11 @@ module DE2_115_BluetoothSPP_Master(
 	input 		          		CLOCK3_50,
 	input 		          		CLOCK_50,
 
-	//////////// Sma //////////
-	input 		          		SMA_CLKIN,
-	output		          		SMA_CLKOUT,
-
-	//////////// LED //////////
-	output		     [8:0]		LEDG,
-	output		    [17:0]		LEDR,
-
 	//////////// KEY //////////
 	input 		     [3:0]		KEY,
 
-	//////////// EX_IO //////////
-	inout 		     [6:0]		EX_IO,
-
 	//////////// SW //////////
 	input 		    [17:0]		SW,
-
-	//////////// SEG7 //////////
-	output		     [6:0]		HEX0,
-	output		     [6:0]		HEX1,
-	output		     [6:0]		HEX2,
-	output		     [6:0]		HEX3,
-	output		     [6:0]		HEX4,
-	output		     [6:0]		HEX5,
-	output		     [6:0]		HEX6,
-	output		     [6:0]		HEX7,
-
-	//////////// LCD //////////
-	output		          		LCD_BLON,
-	inout 		     [7:0]		LCD_DATA,
-	output		          		LCD_EN,
-	output		          		LCD_ON,
-	output		          		LCD_RS,
-	output		          		LCD_RW,
-
-	//////////// RS232 //////////
-	input 		          		UART_CTS,
-	output		          		UART_RTS,
-	input 		          		UART_RXD,
-	output		          		UART_TXD,
-
-	//////////// PS2 for Keyboard and Mouse //////////
-	inout 		          		PS2_CLK,
-	inout 		          		PS2_CLK2,
-	inout 		          		PS2_DAT,
-	inout 		          		PS2_DAT2,
-
-	//////////// SDCARD //////////
-	output		          		SD_CLK,
-	inout 		          		SD_CMD,
-	inout 		     [3:0]		SD_DAT,
-	input 		          		SD_WP_N,
-
-	//////////// VGA //////////
-	output		          		VGA_BLANK_N,
-	output		     [7:0]		VGA_B,
-	output		          		VGA_CLK,
-	output		     [7:0]		VGA_G,
-	output		          		VGA_HS,
-	output		     [7:0]		VGA_R,
-	output		          		VGA_SYNC_N,
-	output		          		VGA_VS,
-
-	//////////// Audio //////////
-	input 		          		AUD_ADCDAT,
-	inout 		          		AUD_ADCLRCK,
-	inout 		          		AUD_BCLK,
-	output		          		AUD_DACDAT,
-	inout 		          		AUD_DACLRCK,
-	output		          		AUD_XCK,
-
-	//////////// I2C for EEPROM //////////
-	output		          		EEP_I2C_SCLK,
-	inout 		          		EEP_I2C_SDAT,
-
-	//////////// I2C for Audio Tv-Decoder  //////////
-	output		          		I2C_SCLK,
-	inout 		          		I2C_SDAT,
-
-	//////////// Ethernet 0 //////////
-	output		          		ENET0_GTX_CLK,
-	input 		          		ENET0_INT_N,
-	input 		          		ENET0_LINK100,
-	output		          		ENET0_MDC,
-	inout 		          		ENET0_MDIO,
-	output		          		ENET0_RST_N,
-	input 		          		ENET0_RX_CLK,
-	input 		          		ENET0_RX_COL,
-	input 		          		ENET0_RX_CRS,
-	input 		     [3:0]		ENET0_RX_DATA,
-	input 		          		ENET0_RX_DV,
-	input 		          		ENET0_RX_ER,
-	input 		          		ENET0_TX_CLK,
-	output		     [3:0]		ENET0_TX_DATA,
-	output		          		ENET0_TX_EN,
-	output		          		ENET0_TX_ER,
-	input 		          		ENETCLK_25,
-
-	//////////// Ethernet 1 //////////
-	output		          		ENET1_GTX_CLK,
-	input 		          		ENET1_INT_N,
-	input 		          		ENET1_LINK100,
-	output		          		ENET1_MDC,
-	inout 		          		ENET1_MDIO,
-	output		          		ENET1_RST_N,
-	input 		          		ENET1_RX_CLK,
-	input 		          		ENET1_RX_COL,
-	input 		          		ENET1_RX_CRS,
-	input 		     [3:0]		ENET1_RX_DATA,
-	input 		          		ENET1_RX_DV,
-	input 		          		ENET1_RX_ER,
-	input 		          		ENET1_TX_CLK,
-	output		     [3:0]		ENET1_TX_DATA,
-	output		          		ENET1_TX_EN,
-	output		          		ENET1_TX_ER,
-
-	//////////// TV Decoder //////////
-	input 		          		TD_CLK27,
-	input 		     [7:0]		TD_DATA,
-	input 		          		TD_HS,
-	output		          		TD_RESET_N,
-	input 		          		TD_VS,
-
-	//////////// USB 2.0 OTG (Cypress CY7C67200) //////////
-	output		     [1:0]		OTG_ADDR,
-	output		          		OTG_CS_N,
-	inout 		    [15:0]		OTG_DATA,
-	input 		          		OTG_INT,
-	output		          		OTG_RD_N,
-	output		          		OTG_RST_N,
-	output		          		OTG_WE_N,
-
-	//////////// IR Receiver //////////
-	input 		          		IRDA_RXD,
 
 	//////////// SDRAM //////////
 	output		    [12:0]		DRAM_ADDR,
@@ -166,45 +37,15 @@ module DE2_115_BluetoothSPP_Master(
 	output		          		SRAM_UB_N,
 	output		          		SRAM_WE_N,
 
-	//////////// Flash //////////
-	output		    [22:0]		FL_ADDR,
-	output		          		FL_CE_N,
-	inout 		     [7:0]		FL_DQ,
-	output		          		FL_OE_N,
-	output		          		FL_RST_N,
-	input 		          		FL_RY,
-	output		          		FL_WE_N,
-	output		          		FL_WP_N,
-
-
 	//////////// GPIO, GPIO connect to RFS - RF and Sensor //////////
 	inout 		          		BT_KEY,
 	input 		          		BT_UART_RX,
 	output		          		BT_UART_TX,
-	input 		          		LSENSOR_INT,
-	inout 		          		LSENSOR_SCL,
-	inout 		          		LSENSOR_SDA,
-	output		          		MPU_AD0_SDO,
-	output		          		MPU_CS_n,
-	output		          		MPU_FSYNC,
-	input 		          		MPU_INT,
-	inout 		          		MPU_SCL_SCLK,
-	inout 		          		MPU_SDA_SDI,
-	input 		          		RH_TEMP_DRDY_n,
-	inout 		          		RH_TEMP_I2C_SCL,
-	inout 		          		RH_TEMP_I2C_SDA,
-	inout 		     [7:0]		TMD_D,
+
 	input 		          		UART2USB_CTS,
 	output		          		UART2USB_RTS,
 	input 		          		UART2USB_RX,
 	output		          		UART2USB_TX,
-	output		          		WIFI_EN,
-	output		          		WIFI_RST_n,
-	input 		          		WIFI_UART0_CTS,
-	output		          		WIFI_UART0_RTS,
-	input 		          		WIFI_UART0_RX,
-	output		          		WIFI_UART0_TX,
-	input 		          		WIFI_UART1_RX
 );
 
 
